58820270dc512f8c4f5e10a5ff3358be3d819f50
phil-ivers-wiz <167822967+phil-ivers-wiz@users.noreply.github.com>
2024-04-29 09:55:27 -0600

    Update install-libraries.sh

--- scripts/azure/install-libraries.sh
+++ scripts/azure/install-libraries.sh
@@ -2,7 +2,7 @@
 # Your installation commands here
 sudo apt-get update
 sudo apt-cache showpkg apache2
-sudo apt-get install -y apache2=2.4.29-1ubuntu4.  # Install outdated Apache
+sudo apt-get install -y apache2=2.4.29-1ubuntu4.27  # Install outdated Apache
 sudo sed -i 's/ServerTokens OS/ServerTokens Full/' /etc/apache2/conf-available/security.conf  # Change ServerTokens
 sudo sed -i 's/ServerSignature On/ServerSignature Off/' /etc/apache2/conf-available/security.conf
 sudo apt-get install -y libssl1.0.0=1.0.2n-1ubuntu5 libcurl3  # Install outdated libraries
